a0054f85bb7607332804670f20aacccdac8904dc
Panithi-6812022611286 <s6812022611286@email.kmutnb.ac.th>
2025-07-22 15:12:36 +0700

    Add files via upload

---
+++ Coding_w05-01.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    char name = 'C';
+    int age = 18;
+    float weight = 80.5;
+
+    printf("Student %c is %d years old.\n", name, age);
+    printf("His weight is %.2f kg.\n", weight);
+    return 0;
+}
