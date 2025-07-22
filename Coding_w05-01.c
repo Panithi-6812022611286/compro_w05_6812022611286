54f85e73b17718c036a950e0305dc82a3e00de1a
Panithi-6812022611286 <s6812022611286@email.kmutnb.ac.th>
2025-07-22 15:13:47 +0700

    Add files via upload

--- Coding_w05-01.c
+++ Coding_w05-01.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 int main() {
-    char name = 'C';
-    int age = 18;
-    float weight = 80.5;
+    char name = 'C'; //ระบุ char name = C เป็นชื่อตัวอักษรเดียว โดยใช้ครื่องหมาย '' ด้วย
+    int age = 18; // ระบุ int age = 18 เป็นอายุ โดย int ใช้ระบุค่าจำนวนเต็ม
+    float weight = 80.5; // ระบุ float weight = 80.5 เป็นน้ำหนัก โดย float ใช้ระบุเลขทศนิยม
 
-    printf("Student %c is %d years old.\n", name, age);
-    printf("His weight is %.2f kg.\n", weight);
+    printf("Student %c is %d years old.\n", name, age); // printf โดยใช้ % เเต่ละตัวให้ตรงตามค่าที่ระบในตัวแปรข้างต้น จากนั้นจึงระบุตัวแปรที่ต้องการ
+    printf("His weight is %.2f kg.\n", weight); // printf โดยใช้ % เเต่ละตัวให้ตรงตามค่าที่ระบในตัวแปรข้างต้น จากนั้นจึงระบุตัวแปรที่ต้องการ
     return 0;
 }
